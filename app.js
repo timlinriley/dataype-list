d7a61b68045c22d26898c27b53baffbeecae52f5
Riley Timlin <RTimlin@allseasonmovers.com>
2023-01-23 09:00:56 -0500

    initialcommit

---
+++ app.js
@@ -0,0 +1,84 @@
+// Strings
+let str1 = Hello
+let str2 = This
+let str3 = is 
+let str4= a 
+let str5 = list
+let str6 = of
+let str7 = ten
+let str8 = unique
+let str9 = strings 
+let str10 = enjoy
+
+// Integers
+let hello = 1
+let This = 2
+let is = 3
+let a = 4
+let list = 5
+let of = 6
+let ten = 7
+let unique = 8
+let strings = 9
+let enjoy = 10
+
+// Object
+let person = {
+    name: Riley
+    Age: 34
+    }
+let daughter ={
+    Name: Rose
+    age: 1.5
+}
+let favorite sport ={
+    sport: baseball
+    team: mets
+}
+
+let favorite food ={
+    type: italian
+    dish: meatballs
+}
+let wife ={
+    name: Valerie
+    hair: brown
+    }
+
+let hometown = {
+    city: New York
+    borough: Queens
+}
+
+let dog ={
+    name: Ziggy
+    type: dalmation
+}
+
+let birthday ={
+    month: october
+    date: 30
+}
+
+let siblings ={
+    quantity: 3
+    gender: female 
+}
+
+let tv= {
+    mount = wall 
+    size = 55 inch 
+
+}
+// Arrays
+
+const array1 =['book', '1', 'true', 'person']
+const array2 = ['daughter', '2', 'false', 'tv']
+const array3 = ['mets', '3', 'false', 'birthday']
+const array4 = ['dog' , '6', 'black', 'white']
+const array5 = ['cat', '4','nyc', 'true']
+const array6 = ['bird', 'tree', 'food', 'false']
+const array7 = ['pencil' '8', '9','false']
+const array8 = ['girl','boy', 'true', '1' ]
+const array9 = ['headphones', 'cup', 'candle', '10']
+const array10 = [ 'spoon', 'bowl', 'ball', 'true'] 
